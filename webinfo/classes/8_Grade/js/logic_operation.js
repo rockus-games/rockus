9e57aab401bae7e6cb993dc187f00f9cb11d1cf5
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-09-01 11:31:59 +0300

    logicor

--- webinfo/classes/8_Grade/js/logic_operation.js
+++ webinfo/classes/8_Grade/js/logic_operation.js
@@ -1,45 +1,89 @@
-const canvas_no = document.getElementById("logic_no")
-const logic_no_pen = canvas_no.getContext("2d")
+const canvas_no = document.getElementById("logic_no");
+const logic_no_pen = canvas_no.getContext("2d");
 
-canvas_no.width = 300
-canvas_no.height = 300
+canvas_no.width = 300;
+canvas_no.height = 300;
 
-logic_no_pen.beginPath()
-logic_no_pen.moveTo(50,50)
-logic_no_pen.lineTo(125,50)
-logic_no_pen.moveTo(175,50)
-logic_no_pen.arc(150,50,25,0,2*Math.PI)
-logic_no_pen.lineTo(250,50)
-logic_no_pen.lineTo(250,250)
-logic_no_pen.lineTo(175,250)
-logic_no_pen.rect(125,225,50,50)
-logic_no_pen.moveTo(125,250)
-logic_no_pen.lineTo(50,250)
-logic_no_pen.lineTo(50,50)
-logic_no_pen.stroke()
+logic_no_pen.beginPath();
+logic_no_pen.moveTo(50, 50);
+logic_no_pen.lineTo(125, 50);
+logic_no_pen.moveTo(175, 50);
+logic_no_pen.arc(150, 50, 25, 0, 2 * Math.PI);
+logic_no_pen.lineTo(250, 50);
+logic_no_pen.lineTo(250, 250);
+logic_no_pen.lineTo(155, 250);
+logic_no_pen.moveTo(155, 235);
+logic_no_pen.lineTo(155, 265);
+logic_no_pen.moveTo(145, 275);
+logic_no_pen.lineTo(145, 225);
+logic_no_pen.moveTo(145, 250);
+logic_no_pen.lineTo(50, 250);
+logic_no_pen.lineTo(50, 225);
+logic_no_pen.lineTo(65, 200);
+logic_no_pen.moveTo(50, 205);
+logic_no_pen.lineTo(50, 50);
 
 
-const canvas_and = document.getElementById("logic_and")
-const logic_and_pen = canvas_and.getContext("2d")
+logic_no_pen.stroke();
 
-canvas_and.width = 300
-canvas_and.height = 300
-logic_and_pen.beginPath()
-logic_and_pen.moveTo(50,50)
-logic_and_pen.lineTo(125,50)
-logic_and_pen.moveTo(175,50)
-logic_and_pen.arc(150,50,25,0,2*Math.PI)
-logic_and_pen.lineTo(250,50)
-logic_and_pen.lineTo(250,250)
-logic_and_pen.lineTo(225,250)
+const canvas_and = document.getElementById("logic_and");
+const logic_and_pen = canvas_and.getContext("2d");
 
-logic_and_pen.rect(175,225,50,50)
+canvas_and.width = 300;
+canvas_and.height = 300;
+logic_and_pen.beginPath();
+logic_and_pen.moveTo(50, 50);
+logic_and_pen.lineTo(125, 50);
+logic_and_pen.moveTo(175, 50);
+logic_and_pen.arc(150, 50, 25, 0, 2 * Math.PI);
+logic_and_pen.lineTo(250, 50);
+logic_and_pen.lineTo(250, 250);
+logic_and_pen.lineTo(155, 250);
+logic_and_pen.moveTo(155, 235);
+logic_and_pen.lineTo(155, 265);
+logic_and_pen.moveTo(145, 275);
+logic_and_pen.lineTo(145, 225);
+logic_and_pen.moveTo(145, 250);
+logic_and_pen.lineTo(50, 250);
+logic_and_pen.lineTo(50, 225);
+logic_and_pen.lineTo(65, 200);
+logic_and_pen.moveTo(50, 205);
+logic_and_pen.lineTo(50, 150);
+logic_and_pen.lineTo(65, 125);
+logic_and_pen.moveTo(50, 130);
+logic_and_pen.lineTo(50, 50);
 
-logic_and_pen.moveTo(175,250)
-logic_and_pen.lineTo(125,250)
-logic_and_pen.rect(75,225,50,50)
+logic_and_pen.stroke();
 
-logic_and_pen.moveTo(75,250)
-logic_and_pen.lineTo(50,250)
-logic_and_pen.lineTo(50,50)
-logic_and_pen.stroke()
+const canvas_or = document.getElementById("logic_or");
+const logic_or_pen = canvas_or.getContext("2d");
+
+canvas_or.width = 300;
+canvas_or.height = 300;
+logic_or_pen.beginPath();
+logic_or_pen.moveTo(50, 50);
+logic_or_pen.lineTo(125, 50);
+logic_or_pen.moveTo(175, 50);
+logic_or_pen.arc(150, 50, 25, 0, 2 * Math.PI);
+logic_or_pen.lineTo(250, 50);
+logic_or_pen.lineTo(250, 250);
+logic_or_pen.lineTo(155, 250);
+logic_or_pen.moveTo(155, 235);
+logic_or_pen.lineTo(155, 265);
+logic_or_pen.moveTo(145, 275);
+logic_or_pen.lineTo(145, 225);
+logic_or_pen.moveTo(145, 250);
+logic_or_pen.lineTo(100, 250);
+logic_or_pen.lineTo(100, 225);
+logic_or_pen.lineTo(115, 200);
+logic_or_pen.moveTo(100, 205);
+logic_or_pen.lineTo(100, 50);
+logic_or_pen.moveTo(100, 250);
+logic_or_pen.lineTo(50, 250);
+logic_or_pen.lineTo(50, 225);
+logic_or_pen.lineTo(65, 200);
+logic_or_pen.moveTo(50, 205);
+logic_or_pen.lineTo(50, 150);
+logic_or_pen.lineTo(50, 50);
+
+logic_or_pen.stroke();
