00c69c1ab776602c0bc708a9f0462143b55915ef
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-10-19 14:02:54 +0300

    123

--- webinfo/professions/index.js
+++ webinfo/professions/index.js
@@ -1,8 +1,12 @@
 let pedagogy = document.querySelector(".pedagogy");
+let IT = document.querySelector(".IT");
 $.getJSON("./index.json", (jsonData) => {
   var json = jsonData;
 
   for (var i = 0; i < json["pedagogy"].length; i++) {
     pedagogy.innerHTML += `<div class="item">${json["pedagogy"][i]}</div>`;
+  }
+  for (var i = 0; i < json["IT"].length; i++) {
+    pedagogy.innerHTML += `<div class="item">${json["IT"][i]}</div>`;
   }
 });
