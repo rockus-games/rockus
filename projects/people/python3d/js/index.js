07bd2f9e1079656f2fd438a7125975d8d9b226b0
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-10-27 12:10:07 +0300

    threejs

--- projects/people/python3d/js/index.js
+++ projects/people/python3d/js/index.js
@@ -2,9 +2,6 @@ import * as THREE from "three";
 import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 
-const controls = new OrbitControls(camera, renderer.domElement);
-const loader = new GLTFLoader();
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -16,9 +13,11 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x181818});
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
