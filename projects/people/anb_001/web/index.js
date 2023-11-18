f9f686f19cc82625d1cd9ec2598f18efdcd1541f
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-11-18 21:36:04 +0300

    space

---
+++ projects/people/anb_001/web/index.js
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth*0.7, window.innerHeight*0.7);
+document. querySelector(".space").appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 1;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
