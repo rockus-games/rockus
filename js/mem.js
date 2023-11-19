93311c033f0040501baa3cd20b248b97e775aa4d
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-11-19 22:52:02 +0300

    Meme

---
+++ js/mem.js
@@ -0,0 +1,10 @@
+let mem = document.querySelector(".mem");
+
+function animate() {
+  mem.style.left = Math.random() * 0.9 * window.innerWidth + "px";
+  mem.style.top = Math.random() * 0.9 * window.innerHeight + "px";
+  setTimeout(() => {
+    requestAnimationFrame(animate);
+  }, 1000);
+}
+animate();
