5b2c8448b35168839547081b18eb1a49eb699ed5
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-11-20 11:41:20 +0300

    list in pe

--- projects/people/PE/index.js
+++ projects/people/PE/index.js
@@ -3,12 +3,14 @@ let p1 = document.querySelector(".p1");
 var json;
 $.getJSON("./data.json", (jsonData) => {
   json = jsonData;
-
+  list.innerHTML += `<a href="#start" ><div id="start" ></div></a>`;
   for (var i in json["names"]) {
     list.innerHTML += `<a onclick="p('${i}')"><div class="item">${i}</div></a>`;
   }
+  list.innerHTML += `<a href="#start" ><div id="end" class="item">В начало</div></a>`;
 });
 
 function p(i) {
   p1.innerHTML = json["names"][i];
 }
+
