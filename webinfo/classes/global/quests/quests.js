2b9a1f7c69ce68ccf508ece0bf7064996219fe3b
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-12-17 10:52:16 +0300

    New quest

--- webinfo/classes/global/quests/quests.js
+++ webinfo/classes/global/quests/quests.js
@@ -1,26 +1,43 @@
 let timer = document.querySelector(".timer");
-let span = document.querySelector("#button");
-function play() {
-  // location.reload();
+let span_div = document.querySelector(".span_div");
+let time_input = document.getElementById("time_input");
+let pause_button = `<span onclick="pause_iteration()" class="material-icons play">pause</span>`;
+let play_button = `<span onclick="play_iteration()" class="material-icons play">play_arrow</span>`;
 
-  for (let i = 0; i < 61; i++) {
-    setTimeout(function () {
-      span.innerHTML = `<span onclick="pause()" class="material-icons play">pause</span>`;
-      // timer.innerHTML = "";
-      timer.style.opacity =1- i*0.1;
-      console.log(timer.style.opacity);
-      timer.innerHTML = i;
-    }, i * 1000);
-  }
+let i = 0;
+let interval;
+
+function play_iteration() {
+  // location.reload();
+  span_div.innerHTML = pause_button;
+  span_div.style.display = "none";
+  interval = setInterval(() => {
+    i++;
+    timer.style.opacity = 1 - i * 0.1;
+    timer.innerHTML = i;
+    if (i == 60) {
+      location.reload();
+    }
+    if (i == 11) {
+      span_div.style.display = "inherit";
+    }
+  }, 1000);
 }
 
-function pause() {
-  span.innerHTML = `<span onclick="play()" class="material-icons play">play_arrow</span>`;
+function pause_iteration() {
+  time_input.style.opacity = 1;
+  span_div.innerHTML = "";
+  clearInterval(interval);
+  console.log(i);
 }
 
 function check() {
-  let time = document.getElementById("time_input").value;
-  if (time > 10) {
+  // console.log(timer.textContent);
+  if (time_input.value != timer.textContent) {
     alert("Все очень плохо");
-}
+    location.reload();
+  } else {
+    alert("Все хорошо");
+    window.location.href = "https://Rockus.su/";
+  }
 }
