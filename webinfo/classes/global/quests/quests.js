74f45c814a67650e79accd5b16bfcfea35f761be
Rockus <45708933+Rockus-com@users.noreply.github.com>
2023-12-17 09:32:41 +0300

    check

--- webinfo/classes/global/quests/quests.js
+++ webinfo/classes/global/quests/quests.js
@@ -1,18 +1,26 @@
 let timer = document.querySelector(".timer");
 let span = document.querySelector("#button");
 function play() {
-    // location.reload();
+  // location.reload();
 
   for (let i = 0; i < 61; i++) {
     setTimeout(function () {
-        span.innerHTML = `<span onclick="reload()" class="material-icons play">pause</span>`;
-        // timer.innerHTML = "";
-        console.log(i);
+      span.innerHTML = `<span onclick="pause()" class="material-icons play">pause</span>`;
+      // timer.innerHTML = "";
+      timer.style.opacity =1- i*0.1;
+      console.log(timer.style.opacity);
       timer.innerHTML = i;
     }, i * 1000);
   }
 }
 
-function reload() {
-  location.reload();
+function pause() {
+  span.innerHTML = `<span onclick="play()" class="material-icons play">play_arrow</span>`;
+}
+
+function check() {
+  let time = document.getElementById("time_input").value;
+  if (time > 10) {
+    alert("Все очень плохо");
+}
 }
