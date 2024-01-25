1a5e29695ec41ced8512c006cbd5c7508455dbf0
Rockus <45708933+Rockus-com@users.noreply.github.com>
2024-01-25 09:07:48 +0300

    mafia

--- projects/people/Mafia/js/index.js
+++ projects/people/Mafia/js/index.js
@@ -1,6 +1,22 @@
 let recommendation = document.querySelector("#recommendation");
+let flag = true
+// function show(){
+//     if(flag == true){
+//         recommendation.style.display = "block";
+//         flag = false
+//     }
+//     else{
+//         recommendation.style.display = "none";
+//         flag = true
+//     }
+// }
 
 function show(){
     recommendation.style.display = "block";
-    console.log(recommendation)
 }
+
+function hide(){
+    recommendation.style.display = "none";
+}
+
+// if (recommendation.sty)
