3a71f57277fb1b4b36a90cc9486cc32a3a82caa0
Rockus <45708933+Rockus-com@users.noreply.github.com>
2024-02-17 15:16:06 +0300

    mafia

--- projects/people/Mafia/js/password.js
+++ projects/people/Mafia/js/password.js
@@ -1,13 +1,5 @@
-let password = document.querySelector("#password");
-let eye1 = document.querySelector("#eye1")
+let alert=document.querySelector("#alert");
 
-
-function show() {
-    if (password.type === "password") {
-        password.type = "text";
-        eye1.innerHTML = "visibility"
-    } else {
-        password.type = "password";
-        eye1.innerHTML = "visibility_off"
-    }
+function show_alert(){
+    alert.style.opacity="1";
 }
