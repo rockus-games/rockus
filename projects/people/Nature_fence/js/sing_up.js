b6bf27fa3ebc6bac161114a715c9ff96645f47f4
Rockus <eduardtepanosyan@gmail.com>
2024-05-11 19:26:24 +0300

    update

--- projects/people/Nature_fence/js/sing_up.js
+++ projects/people/Nature_fence/js/sing_up.js
@@ -7,7 +7,7 @@ let confirm_password = document.querySelector("#password");
 var socket = io("192.168.1.5:3000", {transports: ["websocket"]});
 let data = {};
 
-function sign_up() {
+function sign_up(metka) {
   data = {
     login: nick.value,
     tel: tel.value,
@@ -15,12 +15,12 @@ function sign_up() {
     password: password.value,
     confirm_password: confirm_password.value,
   };
-  socket.emit("david_create_user", data);
+  socket.emit("david",metka, data);
 }
 
-function test_log() {
-  socket.emit("test_log", data);
-}
-socket.on("sign up result", (data) => {
-  alert(data);
-});
+// function test_log() {
+//   socket.emit("test_log", data);
+// }
+// socket.on("sign up result", (data) => {
+//   alert(data);
+// });
