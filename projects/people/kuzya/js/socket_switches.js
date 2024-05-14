a7bb930dab8f26d956add4b5972f71de05ad1bda
Rockus <45708933+Rockus-com@users.noreply.github.com>
2024-05-14 10:47:02 +0300

    last

--- projects/people/kuzya/js/socket_switches.js
+++ projects/people/kuzya/js/socket_switches.js
@@ -3,15 +3,28 @@ let socket_menu = document.querySelector(".socket_menu");
 for (var i = 0; i < 10; i++) {
   socket_switches.innerHTML += `<div id="socket ${
     i + 1
-  }" onclick="single_switch(${i + 1})" class="single_switch" ><span>Розетка ${
+  }" onclick="socket_switch(${i + 1})" class="socket_item" id="socket_item${
     i + 1
-  }</span></div>`;
+  }" ><span>Розетка ${i + 1}</span></div>`;
 }
 socket_menu.innerHTML = `<p id="socket_info" >Информация по розеткам</p>`;
-function single_switch() {
+var socket_info = document.querySelector("#socket_info");
+function socket_switch(id) {
+  socket_info.innerText = `Розетка ${id} нажата`;
   socket.emit("socket_switch");
-  // socket.on("turn_on", function (data) {
-  //    let socket = document.querySelector(`#socket${socket_id}`);
-  //    socket.style.backgroundColor = "green";
-  // })
 }
+
+// for (var i = 0; i < 10; i++) {
+//   socket_switches.innerHTML += `<div id="socket${
+//     i + 1
+//   }" onclick="socket_switch(${i + 1})" id="socket_item${i+1}" ><span>Розетка ${
+//     i + 1
+//   }</span></div>`;
+// }
+// socket_menu.innerHTML = "<p id="socket_info" >Информация по розеткам</p>";
+// var socket_info = document.querySelector("#socket_info");
+// function socket_switch(id) {
+//   socket_info.innerText = "Розетка ${id} нажата";
+//   socket.emit("socket_switch");
+
+// }
