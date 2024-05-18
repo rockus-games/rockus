3314a2cc532cbbd737a5f77016814c8fd258fc0a
Rockus <eduardtepanosyan@gmail.com>
2024-05-18 12:03:38 +0300

    nf

--- projects/people/Nature_fence/js/sing_up.js
+++ projects/people/Nature_fence/js/sing_up.js
@@ -2,25 +2,28 @@ let nick = document.querySelector("#nick");
 let tel = document.querySelector("#tel");
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
-let confirm_password = document.querySelector("#password");
+let confirm_paswword = document.querySelector("#password");
 
-var socket = io("192.168.1.5:3000", {transports: ["websocket"]});
+var socket = io("192.168.0.101:3000", { transports: ["websocket"] });
 let data = {};
 
-function sign_up(metka) {
-  data = {
-    login: nick.value,
-    tel: tel.value,
-    email: email.value,
-    password: password.value,
-    confirm_password: confirm_password.value,
-  };
-  socket.emit("david",metka, data);
-}
+function sign_up(metka){
+    data = {
+        login:nick.value,
+        tel:tel.value,
+        email:email.value,
+        password:password.value,
+        confirm_paswword:confirm_paswword.value
+    };
+    socket.emit("david",metka,data);
+} 
 
-// function test_log() {
-//   socket.emit("test_log", data);
+// function test_log(){
+//     socket.emit("test_log",data)
 // }
-// socket.on("sign up result", (data) => {
-//   alert(data);
-// });
+function hello(metka){
+    data = "empty";
+    socket.emit("david",metka,data);
+    console.log(metka)
+
+}
