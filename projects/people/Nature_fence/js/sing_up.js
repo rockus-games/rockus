d35f165be08d944874feb83d02e755713abe2f1c
Rockus <eduardtepanosyan@gmail.com>
2024-05-26 14:01:45 +0300

    server david

--- projects/people/Nature_fence/js/sing_up.js
+++ projects/people/Nature_fence/js/sing_up.js
@@ -4,26 +4,41 @@ let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let confirm_paswword = document.querySelector("#password");
 
-var socket = io("192.168.0.101:3000", { transports: ["websocket"] });
+var socket = io("192.168.0.103:3000", { transports: ["websocket"] });
 let data = {};
 
-function sign_up(metka){
-    data = {
-        login:nick.value,
-        tel:tel.value,
-        email:email.value,
-        password:password.value,
-        confirm_paswword:confirm_paswword.value
-    };
-    socket.emit("david",metka,data);
-} 
+function sign_up(metka) {
+  data = {
+    login: nick.value,
+    tel: tel.value,
+    email: email.value,
+    password: password.value,
+    confirm_paswword: confirm_paswword.value,
+  };
+  socket.emit("david", metka, data);
+}
+let enter_block = document.querySelector(".enter_block");
+let main = document.querySelector(".main");
+let sign_up_button = document.querySelector("#sign_up_button");
+function show_enter_block() {
+  enter_block.style.display = "flex";
+  main.style.display = "none";
+  sign_up_button.style.display="none";
+}
 
-// function test_log(){
-//     socket.emit("test_log",data)
-// }
-function hello(metka){
-    data = "empty";
-    socket.emit("david",metka,data);
-    console.log(metka)
-
+let login_enter = document.querySelector("#login_enter");
+let password_enter = document.querySelector("#password_enter");
+function save_load() {
+  data = {
+    login: login_enter.value,
+    password: password_enter.value,
+  };
+  socket.emit("david", "check_user", data);
+  socket.once("user_exist", (data) => {
+    if (data) {
+      alert("Пользователь существует");
+    } else {
+      alert("Пользователь не существует");
+    }
+  });
 }
