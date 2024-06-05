e316c05057fb9992ab1f5a005db82c616b9191f5
Rockus <eduardtepanosyan@gmail.com>
2024-06-05 15:03:35 +0300

    pelmen store

--- projects/people/Pelmen_Store/js/header.js
+++ projects/people/Pelmen_Store/js/header.js
@@ -18,5 +18,15 @@ header.innerHTML += `<div class="header_1floor"></div>`;
 let header_1floor = document.querySelector(".header_1floor");
 header_1floor.innerHTML += `<div class="name"></div>`;
 let name = document.querySelector(".name");
-name.innerHTML = `<h1>Pelmen Store</h1>`;
+name.innerHTML = `<a href="./index.html"><h1>Pelmen Store</h1></a>`;
 header_1floor.innerHTML += `<a class="header_1floor_a" href="./store.html">Магазин</a>`;
+
+body.innerHTML += `<div class="nav"></div>`;
+
+let nav = document.querySelector(".nav");
+
+nav.innerHTML += `<a href="./lk.html"><div><span class="nav_span">lk</span></div></a>`;
+nav.innerHTML += `<a href="./new_items.html"><div><span class="nav_span">Новинки</span></div></a>`;
+nav.innerHTML += `<a href="./categories.html"><div><span class="nav_span">Категории</span></div></a>`;
+nav.innerHTML += `<a href="./news.html"><div><span class="nav_span">Новости</span></div></a>`;
+nav.innerHTML += `<input placeholder="Пойск" type="text"/>`;
