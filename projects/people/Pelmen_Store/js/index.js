4cebb63504c3d437cdbaf6023c09d248232b9038
Rockus <eduardtepanosyan@gmail.com>
2024-06-20 12:26:00 +0300

    Pelmen store snapping smooth scroll

--- projects/people/Pelmen_Store/js/index.js
+++ projects/people/Pelmen_Store/js/index.js
@@ -1,17 +1,44 @@
 let recommended = document.querySelector(".recommended");
 
-for (var i = 0; i<10; i++){
-recommended.innerHTML += `<div class="recommended_item"></div>`;
+for (var i = 0; i < 10; i++) {
+  recommended.innerHTML += `<div class="recommended_item">${i}</div>`;
 }
 
 let special_items = document.querySelector(".special_items");
 
-for (var i = 0; i<10; i++){
-    special_items.innerHTML += `<div class="recommended_item"></div>`
+for (var i = 0; i < 10; i++) {
+  special_items.innerHTML += `<div class="recommended_item"></div>`;
 }
 
-let categories = document.querySelector (".categories");
+let categories = document.querySelector(".categories");
 
-for (var i = 0; i<10; i++){
-    categories.innerHTML += `<div class="recommended_item"></div>`
+for (var i = 0; i < 10; i++) {
+  categories.innerHTML += `<div class="recommended_item"></div>`;
+}
+
+var current_recommended = 0;
+function scroll_left_recommended() {
+  if (current_recommended == 0) return;
+
+  current_recommended -= 1;
+
+  let recommended = document.querySelector(".recommended");
+
+  recommended.scrollTo({
+    left: recommended.children[current_recommended].offsetLeft,
+    behavior: "smooth",
+  });
+}
+
+function scroll_right_recommended() {
+  if (current_recommended == 9) return;
+
+  current_recommended += 1;
+
+  let recommended = document.querySelector(".recommended");
+
+  recommended.scrollTo({
+    left: recommended.children[current_recommended].offsetLeft,
+    behavior: "smooth",
+  });
 }
