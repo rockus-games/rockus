9857e0f668605421ff8f0dc525aeca11b972a398
Rockus <eduardtepanosyan@gmail.com>
2024-06-25 15:13:41 +0300

    pe

--- projects/people/Pelmen_Store/js/index.js
+++ projects/people/Pelmen_Store/js/index.js
@@ -1,57 +1,58 @@
 let recommended = document.querySelector(".recommended");
 
-for (var i = 0; i < 10; i++) {
-  recommended.innerHTML += `<div class="recommended_item">${i}</div>`;
+for (var i = 0; i<10; i++){
+recommended.innerHTML += `<div class="recommended_item">${i+1}</div>`;
 }
 
 let special_items = document.querySelector(".special_items");
 
-for (var i = 0; i < 10; i++) {
-  special_items.innerHTML += `<div class="recommended_item"></div>`;
+for (var i = 0; i<10; i++){
+    special_items.innerHTML += `<div class="recommended_item"></div>`
 }
 
-let categories = document.querySelector(".categories");
+let categories = document.querySelector (".categories");
 
-for (var i = 0; i < 10; i++) {
-  categories.innerHTML += `<div class="recommended_item"></div>`;
+for (var i = 0; i<10; i++){
+    categories.innerHTML += `<div class="recommended_item"></div>`
 }
 
-let current_recommended = 0;
-function scroll_left_recommended() {
-  if (current_recommended == 0) return;
+var current_recommended = 0
 
-  current_recommended -= 1;
+function left_arrow_recommended(){
+    if (current_recommended == 0) return;
 
-  // let recommended = document.querySelector(".recommended");
+    current_recommended -= 1;
 
-  recommended.scrollTo({
-    left: recommended.children[current_recommended].offsetLeft,
-    behavior: "smooth",
-  });
+    recommended.scrollTo({
+        left: recommended.children[current_recommended].offsetLeft,
+        behavior: "smooth",
+    });
+    // console.log(recommended);
+}
+function right_arrow_recommended(){
+    if (current_recommended == 9) return;
+
+    current_recommended += 1;
+
+    recommended.scrollTo({
+        left: recommended.children[current_recommended].offsetLeft,
+        behavior: "smooth",
+    });
+
+    // console.log(recommended);
 }
 
-function scroll_right_recommended() {
-  if (current_recommended == 9) return;
+let buttonchiki = document.querySelector(".buttonchiki");
 
-  current_recommended += 1;
+for (var i = 0; i<10; i++){
+    buttonchiki.innerHTML += `<div onclick="buttonchik_move(${i})" class="buttonchik"></div>`
+};
 
+function buttonchik_move(x){
+    current_recommended = x
 
-  recommended.scrollTo({
-    left: recommended.children[current_recommended].offsetLeft,
-    behavior: "smooth",
-  });
-}
-
-let batonchiki = document.querySelector(".batonchiki");
-
-for (var i = 0; i < 10; i++) {
-  batonchiki.innerHTML += `<div onclick="choose_recommended(${i})" class="batonchik"></div>`;
-}
-
-function choose_recommended(x) {
-  current_recommended = x;
-  recommended.scrollTo({
-    left: recommended.children[current_recommended].offsetLeft,
-    behavior: "smooth",
-  });
+    recommended.scrollTo({
+        left: recommended.children[current_recommended].offsetLeft,
+        // behavior: "smooth",
+    });
 }
