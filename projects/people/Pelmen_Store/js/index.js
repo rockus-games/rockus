1f97032fe3a3e4b17418d9c49c70a924b836a01a
Rockus <eduardtepanosyan@gmail.com>
2024-07-02 16:36:00 +0300

    ps

--- projects/people/Pelmen_Store/js/index.js
+++ projects/people/Pelmen_Store/js/index.js
@@ -1,14 +1,27 @@
 let recommended = document.querySelector(".recommended");
 
-for (var i = 0; i < 10; i++) {
-  recommended.innerHTML += `<div class="recommended_item">${i}</div>`;
-}
-let recommended_item = document.querySelector(".recommended_item");
+let recommended_game_count = 2;
 
-recommended_item.innerHTML += `<div class="poster"></div>`;
-recommended_item.innerHTML += `<div class="poster_images"></div>`;
-let poster = document.querySelector(".poster");
-poster.innerHTML += `<img width=200 src="./assets/images/recommended/image1.jpeg" alt="" />`;
+for (var i = 0; i < recommended_game_count; i++) {
+  recommended.innerHTML += `<div id="recommended_game${i+1}" class="recommended_item"></div>`;
+  let recommended_game = document.querySelector(`#recommended_game${i+1}`);
+  recommended_game.innerHTML += `<div id="poster${i+1}" class="poster"></div>`;
+  let poster = document.querySelector(`#poster${i+1}`);
+  recommended_game.innerHTML += `<div id="poster_images${i+1}" class="poster_images"></div>`;
+  let poster_images = document.querySelector(`#poster_images${i+1}`);
+  for (var j = 0; j < 4; j++){
+    poster.innerHTML = `<img src="./assets/images/recommended/game${i+1}/image${j+1}.jpeg"></img>`;
+    poster_images.innerHTML += `<div id="cartochka${j+1}" class="cartochki"></div>`
+  }
+}
+
+// let recommended_item = document.querySelector(".recommended_item");
+// recommended_item.innerHTML += `<div class="poster"></div>`;
+// recommended_item.innerHTML += `<div class="poster_images"></div>`;
+
+// let poster = document.querySelector(".poster");
+// poster.innerHTML = `<img src="./assets/images/recommended/game1/image1.jpeg"></img>`
+
 let special_items = document.querySelector(".special_items");
 
 for (var i = 0; i < 10; i++) {
@@ -27,6 +40,7 @@ function left_arrow_recommended() {
   if (current_recommended == 0) return;
 
   current_recommended -= 1;
+
   select_buttonchik(current_recommended);
 
   recommended.scrollTo({
@@ -36,11 +50,12 @@ function left_arrow_recommended() {
   // console.log(recommended);
 }
 function right_arrow_recommended() {
-  if (current_recommended == 9) return;
+  if (current_recommended == recommended_game_count-1) return;
 
   current_recommended += 1;
+
   select_buttonchik(current_recommended);
-  // console.log(current_recommended);
+
   recommended.scrollTo({
     left: recommended.children[current_recommended].offsetLeft,
     behavior: "smooth",
@@ -51,23 +66,26 @@ function right_arrow_recommended() {
 
 let buttonchiki = document.querySelector(".buttonchiki");
 
-for (var i = 0; i < 10; i++) {
-  buttonchiki.innerHTML += `<div id="buttonchik${i}" onclick="buttonchik_move(${i})" class="buttonchik"></div>`;
+for (var i = 0; i < recommended_game_count; i++) {
+  buttonchiki.innerHTML += `<div onclick="buttonchik_move(${i})" id="buttonchik${i}" class="buttonchik"></div>`;
 }
 
 function buttonchik_move(x) {
   current_recommended = x;
+
   select_buttonchik(current_recommended);
-  // console.log(current_recommended);
+
   recommended.scrollTo({
     left: recommended.children[current_recommended].offsetLeft,
     // behavior: "smooth",
   });
 }
-let current_buttonchik = document.querySelector(`#buttonchik0`);
-current_buttonchik.style.backgroundColor = "red";
+
+let current_buttonchik = document.querySelector("#buttonchik0");
+
 function select_buttonchik(x) {
   current_buttonchik.style.backgroundColor = "#293d4e";
+  //   current_buttonchik.style.hover = "background-color: #294c4e transition: all 0.3s;"
   current_buttonchik = document.querySelector(`#buttonchik${x}`);
-  current_buttonchik.style.backgroundColor = "red";
+  current_buttonchik.style.backgroundColor = "#3f605f";
 }
