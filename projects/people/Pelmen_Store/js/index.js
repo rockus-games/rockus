cd3fd52b72c96eb799acd527e17bbcca03849d48
Rockus <eduardtepanosyan@gmail.com>
2024-07-08 15:58:54 +0300

    pe

--- projects/people/Pelmen_Store/js/index.js
+++ projects/people/Pelmen_Store/js/index.js
@@ -1,30 +1,18 @@
 let recommended = document.querySelector(".recommended");
-var current_recommended = 0;
 
-let recommended_game_count = 2;
+let recommended_game_count = 9;
 
 for (var i = 0; i < recommended_game_count; i++) {
   recommended.innerHTML += `<div id="recommended_game${i+1}" class="recommended_item"></div>`;
   let recommended_game = document.querySelector(`#recommended_game${i+1}`);
   recommended_game.innerHTML += `<div id="poster${i+1}" class="poster"></div>`;
   let poster = document.querySelector(`#poster${i+1}`);
-  poster.innerHTML = `<img src="./assets/images/recommended/game${i+1}/image1.jpeg"></img>`;
-
+  poster.innerHTML = `<img src="./assets/images/recommended/game${i+1}/image1.jpeg">`;
   recommended_game.innerHTML += `<div id="poster_images${i+1}" class="poster_images"></div>`;
   let poster_images = document.querySelector(`#poster_images${i+1}`);
-
   for (var j = 0; j < 4; j++){
-    poster_images.innerHTML += `<div  onmouseover="cartochka_hover(${j+2})" onmouseout="cartochka_out()" id="cartochka${j+1}" class="cartochki"><img width="120" height="120" src="./assets/images/recommended/game${i+1}/image${j+2}.jpeg"></img></div>`;
+    poster_images.innerHTML += `<div onmouseover="poster_image_change(${j+2})" onmouseout="poster_image_unchange()" id="cartochka${j+1}" class="cartochki"><img width="100" height="100" src="./assets/images/recommended/game${i+1}/image${j+2}.jpeg"></div>`;
   }
-}
-function cartochka_hover(i) {
-  let poster = document.querySelector(`#poster${current_recommended+1}`);
-  poster.innerHTML = `<img width="672" src="./assets/images/recommended/game${current_recommended+1}/image${i}.jpeg"></img>`;
-}
-
-function cartochka_out() {
-  let poster = document.querySelector(`#poster${current_recommended+1}`);
-  poster.innerHTML = `<img src="./assets/images/recommended/game${current_recommended+1}/image1.jpeg"></img>`;
 }
 
 // let recommended_item = document.querySelector(".recommended_item");
@@ -46,6 +34,7 @@ for (var i = 0; i < 10; i++) {
   categories.innerHTML += `<div class="recommended_item"></div>`;
 }
 
+var current_recommended = 0;
 
 function left_arrow_recommended() {
   if (current_recommended == 0) return;
@@ -99,4 +88,14 @@ function select_buttonchik(x) {
   //   current_buttonchik.style.hover = "background-color: #294c4e transition: all 0.3s;"
   current_buttonchik = document.querySelector(`#buttonchik${x}`);
   current_buttonchik.style.backgroundColor = "#3f605f";
+}
+
+function poster_image_change(x){
+  let poster = document.querySelector(`#poster${current_recommended+1}`);
+  poster.innerHTML = `<img src="./assets/images/recommended/game${current_recommended+1}/image${x}.jpeg">`;
+}
+
+function poster_image_unchange(){
+  let poster = document.querySelector(`#poster${current_recommended+1}`);
+  poster.innerHTML = `<img src="./assets/images/recommended/game${current_recommended+1}/image1.jpeg">`;
 }
