788a43729e01608fe3406bdd88d945929f761ea1
Rockus <45708933+Rockus-com@users.noreply.github.com>
2025-08-18 14:52:40 +0300

    mem

--- js/mem.js
+++ js/mem.js
@@ -3,7 +3,8 @@ let mem = document.querySelector(".mem");
 function animate() {
   mem.style.left = Math.random() * 0.9 * window.innerWidth + "px";
   mem.style.top = Math.random() * 0.9 * window.innerHeight + "px";
-  mem.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+  // mem.style.transform = "rotate(" + Math.random() * 360 + "deg)";
+  mem.style.scale = Math.random() * 2;
   setTimeout(() => {
     requestAnimationFrame(animate);
   }, 700);
